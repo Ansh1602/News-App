65e065349f82c15f289a77b75c83f196e68d6af9
Ansh1602 <ansh.khanna16@gmail.com>
2022-09-30 16:34:21 +0530

    With prev next button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  pageSize = 5;
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <NavBar />
+          {/* <News key="general" pageSize={5} country="in" category="general" /> */}
+          <Routes>
+            <Route exact path="/" element={<News  key="general"  pageSize={this.pageSize}  category="general"></News>}/>
+            <Route exact path="/business" element={<News  key="business"  pageSize={this.pageSize}  category="business"></News>}/>
+            <Route exact path="/entertainment" element={<News  key="entertainment"  pageSize={this.pageSize}  category="entertainment"></News>}/>
+            <Route exact path="/general" element={<News  key="general"  pageSize={this.pageSize}  category="general"></News>}/>
+            <Route exact path="/health" element={<News  key="health"  pageSize={this.pageSize}  category="health"></News>}/>
+            <Route exact path="/science" element={<News  key="science"  pageSize={this.pageSize}  category="science"></News>}/>
+            <Route exact path="/sports" element={<News  key="sports"  pageSize={this.pageSize}  category="sports"></News>}/>
+            <Route exact path="/technology" element={<News  key="technology"  pageSize={this.pageSize}  category="technology"></News>}/>
+          </Routes>
+
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/NewsItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+
+
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: 1}}>
+                            {source}
+                            <span className="visually-hidden">unread messages</span>
+                        </span>
+                    <img src={!imageUrl ? "https://images.indianexpress.com/2022/09/Dark-energy-camera-lobster-nebula.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}   
+                        </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
